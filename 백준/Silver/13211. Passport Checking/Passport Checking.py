5316cac57032f2910090f07530e23d06753af547
Woosuk Nam <me@suk.kr>
2024-01-02 23:25:59 +0900

    [Silver V] Title: Passport Checking, Time: 308 ms, Memory: 128732 KB -BaekjoonHub

--- 백준/Silver/13211. Passport Checking/Passport Checking.py
+++ 백준/Silver/13211. Passport Checking/Passport Checking.py
@@ -1,2 +1,2 @@
-stolen = {input(): 1 for _ in range(int(input()))}
-print(sum([stolen.get(input(), 0) for _ in range(int(input()))]))
+s = {input(): 1 for _ in range(int(input()))}
+print(sum([s.get(input(), 0) for _ in range(int(input()))]))
